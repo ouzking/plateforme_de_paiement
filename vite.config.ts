1fa4f4daaebe978bab6b9b0ca9cbaf45bb20ded4
Ousmane Bodian <ousmanearfangkemobodian@gmail.com>
2025-09-28 01:51:24 +0100

    fix vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/plateforme_de_paiement/", // IMPORTANT : ton repo GitHub
 })
